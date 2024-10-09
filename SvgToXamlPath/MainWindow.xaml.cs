a86a080af118d821309e569929c725e2c010b55e
Rextens <jskudrys@gmail.com>
2024-10-09 15:08:33 +0200

    working for paths, not figures yet

---
+++ SvgToXamlPath/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SvgToXamlPath
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog();
+
+            XamlPathBuilder xamlPathBuilder = new XamlPathBuilder();
+
+            if(folderDialog.ShowDialog() == true)
+            {
+                //MessageBox.Show(folderDialog.FolderName);
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderDialog.FolderName);
+
+                FileInfo[] Files = directoryInfo.GetFiles("*.svg");
+
+                string sum = "";
+
+                foreach (FileInfo file in Files)
+                {
+                    string key = file.Name.Split('.')[0];
+                    XElement newGeometryGroup = xamlPathBuilder.AddGeometryGroup(key);
+
+                    XNamespace xNamespace = "http://www.w3.org/2000/svg";
+                    XDocument svgDocument = XDocument.Load(file.FullName);
+
+                    var arr = svgDocument.Descendants(xNamespace + "path").ToArray();
+
+                    foreach (var node in arr)
+                    {
+                        if(node.Attribute("d") != null)
+                            newGeometryGroup.Add(new XElement("PathGeometry", new XAttribute("Figures", node.Attribute("d").Value)));
+                    }
+                }
+            }
+
+            xamlPathBuilder.Save(folderDialog.FolderName);
+        }
+    }   
+}
---
+++ SvgToXamlPath/XamlPathBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SvgToXamlPath
+{
+    public class XamlPathBuilder
+    {
+        XNamespace x = "http://schemas.microsoft.com/winfx/2006/xaml";
+        XNamespace presentation = "http://schemas.microsoft.com/winfx/2006/xaml/presentation";
+        XDocument xDocument;
+        public XamlPathBuilder()
+        {
+           xDocument = new XDocument(new XElement("root", new XAttribute(XNamespace.Xmlns + "x", x)));
+        }
+
+        public XElement AddGeometryGroup(string Key)
+        {
+            var newGeometryGroup = new XElement("GeometryGroup", new XAttribute(x + "Key", Key));
+
+            xDocument.Root.Add(newGeometryGroup);
+
+            return newGeometryGroup;
+        }
+
+        public void Save(string path)
+        {
+            path += ".xaml";
+            xDocument.Save(path);
+        }
+    }
+}
